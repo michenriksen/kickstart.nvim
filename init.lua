b08afb70498816a2a0bd0dee078d2ea713102aef
Michael Henriksen <mchnrksn@gmail.com>
2023-01-01 09:39:54 +0100

    Install additional treesitter languages and LSPs

--- init.lua
+++ init.lua
@@ -221,10 +221,10 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help' },
+  ensure_installed = { 'bash', 'css', 'dockerfile', 'go', 'help', 'html', 'json', 'lua', 'make', 'markdown', 'ruby', 'toml', 'typescript', 'vue', 'yaml' },
 
   highlight = { enable = true },
-  indent = { enable = true, disable = { 'python' } },
+  indent = { enable = true },
   incremental_selection = {
     enable = true,
     keymaps = {
@@ -337,12 +337,10 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-
+  gopls = {},
+  tsserver = {},
+  solargraph = {},
+  volar = {},
   sumneko_lua = {
     Lua = {
       workspace = { checkThirdParty = false },
